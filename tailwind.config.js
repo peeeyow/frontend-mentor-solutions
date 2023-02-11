6ebe344d691f0e1faebc8dd04a586baf9342dd3d
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-02-11 19:12:11 +0800

    refactor(scope): Restructure project
    This is restructured in such a way building and deplying static site
    will be easier.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,25 @@ module.exports = {
     extend: {
       fontFamily: {
         outfit: ["Outfit", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
       colors: {
-        fm: {
+        qrcc: {
           white: "hsl(0, 0%, 100%)",
           "light-gray": "hsl(212, 45%, 89%)",
           "grayish-blue": "hsl(220, 15%, 55%)",
           "dark-blue": "hsl(218, 44%, 22%)",
+        },
+        home: {
+          white: "#c8ccd4",
+          black: "#282c34",
+          "light-gray": "#abb2bf",
+          yellow: "#e5c07b",
+          "dark-gray": "#353b45",
+          blue: "#61afef",
+          green: "#98c379",
+          red: "#be5046",
+          orange: "#d19a66",
         },
       },
     },
