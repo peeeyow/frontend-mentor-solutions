b6bda69779d171755a22f7ce41cd32f183b1daa7
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-02-12 12:46:45 +0800

    Implement product preview card component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,18 @@ module.exports = {
         outfit: ["Outfit", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
         "red-hat-display": ["Red Hat Display", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
       },
       colors: {
+        ppcc: {
+          "darker-cyan": "hsl(158, 36%, 20%)",
+          "dark-cyan": "hsl(158, 36%, 37%)",
+          cream: "hsl(30, 38%, 92%)",
+          "very-dark-blue": "hsl(212, 21%, 14%)",
+          "dark-grayish-blue": "hsl(228, 12%, 48%)",
+          white: "hsl(0, 0%, 100%)",
+        },
         osc: {
           "pale-blue": "hsl(225, 100%, 94%)",
           "bright-blue": "hsl(245, 75%, 52%)",
